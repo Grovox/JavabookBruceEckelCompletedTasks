e1dc5b4b47fccf8767b185af33d75080609fc78b
Grovox <Grovox@bk.ru>
2022-12-13 14:58:22 +0300

    Finish 14/3 task

---
+++ 14/src/Three.java
@@ -0,0 +1,37 @@
+//: typeinfo/Shapes.java
+import java.util.*;
+
+abstract class Shape {
+    void draw() { System.out.println(this + ".draw()"); }
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    public String toString() { return "Circle"; }
+}
+
+class Square extends Shape {
+    public String toString() { return "Square"; }
+}
+
+class Triangle extends Shape {
+    public String toString() { return "Triangle"; }
+}
+
+class Rhomboid extends Shape {
+    public String toString() { return "Rhomboid"; }
+}
+
+public class Three {
+    public static void main(String[] args) {
+        List<Shape> shapeList = Arrays.asList(
+                new Circle(), new Square(), new Triangle(), new Rhomboid()
+        );
+        for(Shape shape : shapeList)
+            shape.draw();
+    }
+} /* Output:
+Circle.draw()
+Square.draw()
+Triangle.draw()
+*///:~
