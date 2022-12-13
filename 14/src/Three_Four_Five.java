b9b9bbe9a8bcda19688611b7098ab22f963f9368
Grovox <Grovox@bk.ru>
2022-12-13 17:13:59 +0300

    Finish 14/4 14/5 task

--- 14/src/Three_Four_Five.java
+++ 14/src/Three_Four_Five.java
@@ -4,6 +4,12 @@ import java.util.*;
 abstract class Shape {
     void draw() { System.out.println(this + ".draw()"); }
     abstract public String toString();
+
+    void rotate(){
+        if(this instanceof Circle){
+            this.draw();
+        }
+    }
 }
 
 class Circle extends Shape {
@@ -22,16 +28,26 @@ class Rhomboid extends Shape {
     public String toString() { return "Rhomboid"; }
 }
 
-public class Three {
+public class Three_Four_Five {
     public static void main(String[] args) {
         List<Shape> shapeList = Arrays.asList(
                 new Circle(), new Square(), new Triangle(), new Rhomboid()
         );
-        for(Shape shape : shapeList)
+
+        for(Shape shape : shapeList) {
             shape.draw();
+            shape.rotate();
+            System.out.println();
+        }
+        try{
+            if(shapeList.get(3)  instanceof Rhomboid){
+                Rhomboid rhomboid = (Rhomboid) shapeList.get(3);
+                rhomboid.draw();
+            }else throw new Exception();
+        }catch (Exception e){
+            System.out.println("Преобразование не возможно");
+            System.exit(1);
+        }
+        /*Circle circle = (Rhomboid) shapeList.get(3);*/ //нельзя преобразовать
     }
-} /* Output:
-Circle.draw()
-Square.draw()
-Triangle.draw()
-*///:~
+}
