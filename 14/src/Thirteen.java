55fe2d2c00564ba2aad0e4d8d7d7dab4026a23c9
Grovox <Grovox@bk.ru>
2022-12-21 18:05:34 +0300

    Finish 14/13 task

---
+++ 14/src/Thirteen.java
@@ -0,0 +1,23 @@
+import coffee.Coffee;
+import coffee.CoffeeGenerator;
+import factory.*;
+import net.mindview.util.TypeCounter;
+
+import java.util.ArrayList;
+
+public class Thirteen {
+    public static void main(String[] args) {
+        TypeCounter counter = new TypeCounter(Part.class);
+        ArrayList<Part> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(Part.createRandom());
+        }
+        for (Part part : list) {
+            System.out.print(part.getClass().getSimpleName() + " ");
+            counter.count(part);
+        }
+
+        System.out.println();
+        System.out.println(counter);
+    }
+}
